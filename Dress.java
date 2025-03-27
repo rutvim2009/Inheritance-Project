5c6831c07a17a69cfb4b7d62b3a0d0e771559cdc
rutvim2009 <rutvim2009@gmail.com>
2025-03-27 20:16:59 +0000

    created dress subclass

---
+++ Dress.java
@@ -0,0 +1,12 @@
+public class Dress {
+    protected String length;
+    protected String sleeveType;
+    protected String bodice;
+
+
+    @Override
+    public String toString() {
+        return "This is a beautiful dress, with a " + color + " color, made of a " + material + " material. Having a " + length + " length, beautiful " + sleeve + " sleeve style, and a beautiful " + bodice + "bodice style, this piece is graciously given to us by " + brand + "."; 
+
+    }
+}
