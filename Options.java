ad6dfc8d42e7529b33f4946a10746273aed15f3d
S1eepyLeaf <161251082+S1eepyLeaf@users.noreply.github.com>
2025-03-27 20:41:44 +0000

    options hahahahahahahahahahahaha

--- Options.java
+++ Options.java
@@ -4,9 +4,8 @@ public class Options {
     public String[] brands = {"Gucci", "Louis Vuitton", "Prada", "Nike", "Adidas", "H&M", "Zara"};
     public String[] clothingSizes = {"XS", "S", "M", "L", "XL", "XXL", "XXXL"};
     public double[] shoeSizes = {5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14};
-    public String[] patterns = {};
-    public String[] adj = {"formal", "elegant", "sophisticated", "comfortable", "simple", "casual", "fashional", "stylish", "plain", 
-    "baggy", "flowy", "cropped", "beautiful", "vintage"};
+    public String[] patterns = {"stripes", "plaid", "polka dots", "camo", "floral", "tie-dye", "lace", "chevron", "leopard print", "zebra print"};
+    public String[] adj = {"formal", "elegant", "sophisticated", "comfortable", "simple", "casual", "fashional", "stylish", "plain", "beautiful", "vintage"};
 
     
 }
