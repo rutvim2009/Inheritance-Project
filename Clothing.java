37849185a04d6e173e661ed88e78f19daad1b847
rutvim2009 <rutvim2009@gmail.com>
2025-03-27 20:04:00 +0000

    created parent class - rutvi

---
+++ Clothing.java
@@ -0,0 +1,15 @@
+public class Clothing {
+    protected String color;
+    protected String material;
+    protected String brand;
+
+    public Clothing(String c, String m, String b) {
+        color = c;
+        material = m;
+        brand = b;
+    }
+
+    public String toString() {
+        return "This is a beautiful "
+    }
+}
