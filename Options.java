339d7a89e88e482c15bd7fb672139ac7a28bec7f
S1eepyLeaf <161251082+S1eepyLeaf@users.noreply.github.com>
2025-03-28 18:51:33 +0000

    optoins classe

--- Options.java
+++ Options.java
@@ -6,6 +6,4 @@ public class Options {
     public double[] shoeSizes = {5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14};
     public String[] patterns = {"stripes", "plaid", "polka dots", "camo", "floral", "tie-dye", "lace", "chevron", "leopard print", "zebra print"};
     public String[] adj = {"formal", "elegant", "sophisticated", "comfortable", "simple", "casual", "fashional", "stylish", "plain", "beautiful", "vintage"};
-
-    
 }
